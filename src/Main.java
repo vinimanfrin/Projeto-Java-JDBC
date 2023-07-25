c96628d66f70b03680a74798c06d722d70e7bb40
vinimanfrin <viniciusmanfrin2019@gmail.com>
2023-07-25 17:59:31 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import model.entites.Department;
+import model.entites.Seller;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Department teste = new Department(2,"Books");
 
-
-
-
+        Seller sellerTeste = new Seller(21,"Bob","bog@gmail.com",new Date(),1500.0,teste);
+        System.out.println(teste);
+        System.out.println(sellerTeste);
 
 
     }
