8a1dbf50b3d2f7bc1a2b442d9aead7b7bcf9ca87
vinimanfrin <viniciusmanfrin2019@gmail.com>
2023-07-26 17:14:40 -0300

    findByDepartment implementation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.daoImpl.SellerDaoJDBC;
 import model.entites.Department;
 import model.entites.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: findByDepartment ===");
+        Department department = new Department(2,null);
+        List<Seller> list = SellerDaoJDBC.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(seller);
+        }
 
 
     }
--- src/model/daoImpl/SellerDaoJDBC.java
+++ src/model/daoImpl/SellerDaoJDBC.java
@@ -9,15 +9,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
     private Connection conn;
-    public SellerDaoJDBC(Connection conn){
+
+    public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
-
 
 
     @Override
@@ -42,14 +45,14 @@ public class SellerDaoJDBC implements SellerDao {
         try {
             st = conn.prepareStatement(
                     "SELECT seller.*,department.Name as DepName " +
-                        "FROM seller INNER JOIN department " +
-                        "ON seller.DepartmentId = department.Id " +
-                        "WHERE seller.Id = ? ");
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE seller.Id = ? ");
             st.setInt(1, id);
             rs = st.executeQuery();
             if (rs.next()) {
                 Department dep = instantiateDepartment(rs);
-                Seller seller = instantiateSeller(rs,dep);
+                Seller seller = instantiateSeller(rs, dep);
                 return seller;
             }
             return null;
@@ -66,9 +69,54 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
+    public  List<Seller> findByDepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE DepartmentId = ? " +
+                            "ORDER BY Name ");
+            st.setInt(1, department.getId());
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instantiateSeller(rs, dep);
+                list.add(seller);
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+
+        }
+
+    }
+
+
+
+    @Override
     public List<Seller> findAll() {
         return null;
     }
+
+
 
     private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
         Seller seller = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"),
