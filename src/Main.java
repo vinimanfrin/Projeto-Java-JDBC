b86ef01b09c7e39727052e8390cd6ead017c465b
vinimanfrin <viniciusmanfrin2019@gmail.com>
2023-07-26 21:51:52 -0300

    Insert implementation

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,21 @@ public class Main {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-//        System.out.println("\n=== TEST 2: findByDepartment ===");
-//        Department department = new Department(2,null);
-//        List<Seller> list = SellerDao.findByDepartment(department);
-//        for (Seller obj : list) {
-//            System.out.println(obj);
-//        }
-//        System.out.println("\n=== TEST 3: findAll ===");
-//        List<Seller> list = SellerDao.findAll();
-//        for (Seller obj : list) {
-//           System.out.println(obj);
-//       }
-
+        System.out.println("\n=== TEST 2: findByDepartment ===");
+        Department department = new Department(2,null);
+        List<Seller> list = SellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+        System.out.println("\n=== TEST 3: findAll ===");
+        List<Seller> list = SellerDao.findAll();
+        for (Seller obj : list) {
+           System.out.println(obj);
+       }
+        System.out.println("\n=== TEST 4: findAll ===");
+        Seller newSeller = new Seller(null,"Gred","greg@gmail.com",new Date(),4000.0,department);
+        SellerDao.insert(newSeller);
+        System.out.println("Inserted! New id= "+ newSeller.getId());
 
     }
 }
